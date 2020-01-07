2706624cdb80feecafb114cbe341bbade2420778
Vinícius Albuquerque <vinicius.albuquerque@aevo.com.br>
2020-01-07 11:22:38 -0300

    feat: list provider's notifications :sparkles:

--- backend/src/app/controllers/NotificationController.js
+++ backend/src/app/controllers/NotificationController.js
@@ -1,8 +1,28 @@
 import Notification from '../schemas/Notification';
+import User from '../models/User';
 
 class NotificationController {
   async index(req, res) {
-    return res.json();
+    /**
+     * Check if provider_id is a provider
+     */
+    const isProvider = await User.findOne({
+      where: { id: req.userId, provider: true },
+    });
+
+    if (!isProvider) {
+      return res
+        .status(401)
+        .json({ error: 'Only providers can load notifications.' });
+    }
+
+    const notifications = await Notification.find({
+      user: req.userId,
+    })
+      .sort({ createdAt: 'desc' })
+      .limit(20);
+
+    return res.json(notifications);
   }
 }
 
