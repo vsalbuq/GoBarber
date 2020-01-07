d5532f40fcdb4f9717f45fa1524e480e436d01af
Vinícius Albuquerque <vinicius.albuquerque@aevo.com.br>
2020-01-07 13:44:13 -0300

    feat: send email when cancelling appointment :sparkles:

---
+++ backend/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: '9ea30d104c2e56',
+    pass: 'e87af4a3c4f5dd',
+  },
+  secure: false,
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
