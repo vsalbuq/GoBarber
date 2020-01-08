a591b85d876459df1293e53c948268ac8b4eb7c0
Vinícius Albuquerque <vinicius.albuquerque@aevo.com.br>
2020-01-08 15:07:19 -0300

    feat: email templates with handlebars :sparkles:

--- backend/src/app/controllers/AppointmentController.js
+++ backend/src/app/controllers/AppointmentController.js
@@ -121,6 +121,11 @@ class AppointmentController {
           as: 'provider',
           attributes: ['name', 'email'],
         },
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
       ],
     });
 
@@ -145,7 +150,12 @@ class AppointmentController {
     await Mail.sendMail({
       to: `${appointment.provider.name} <${appointment.provider.email}>`,
       subject: 'Appointment cancelled',
-      text: 'You have a new cancellation',
+      template: 'cancellation',
+      context: {
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        date: format(appointment.date, "dd MMMM', 'H:mm'h'"),
+      },
     });
 
     return res.json(appointment);
