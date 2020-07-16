44bc7c5d26f00bc0d7e5770022eb8f0823706a52
Vinícius Albuquerque <vinicius.albuquerque@outlook.com>
2020-07-16 15:55:42 -0300

    feat: Appointments

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 
+import routes from './routes';
+
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World!' });
-});
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('🚀 Server started on port 3333');
