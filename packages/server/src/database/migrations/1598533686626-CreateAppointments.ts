3a1cb267c92d42b302456b017209f37c64cc48d3
Vinícius Albuquerque <vinicius.albuquerque@outlook.com>
2020-08-28 08:07:21 -0300

    feat: Adds relationship between Appointments and Users

--- packages/server/src/database/migrations/1598533686626-CreateAppointments.ts
+++ packages/server/src/database/migrations/1598533686626-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1598533686626
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
