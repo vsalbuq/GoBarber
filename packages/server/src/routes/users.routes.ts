26244ac2d9423bb28fd9e749fe48dfb94571ea7b
Vinícius Albuquerque <vinicius.albuquerque@outlook.com>
2020-09-03 19:01:45 -0300

    feat: Adds user avatar upload

--- packages/server/src/routes/users.routes.ts
+++ packages/server/src/routes/users.routes.ts
@@ -1,13 +1,16 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
 
-const usersRouter = Router();
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
+const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
   try {
-
     const { name, email, password } = request.body;
 
     const createUser = new CreateUserService();
@@ -15,14 +18,22 @@ usersRouter.post('/', async (request, response) => {
     const user = await createUser.execute({
       name,
       email,
-      password
+      password,
     });
 
     return response.json(user);
-
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    return response.json();
+  },
+);
 
 export default usersRouter;
