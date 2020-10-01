45b10c5af0281dedf65c76a379addd382ca5c5ca
Vinícius Albuquerque <vinicius.albuquerque@outlook.com>
2020-10-01 20:15:39 -0300

    feat: Adds error messages to UI

--- packages/web/src/pages/SignUp/index.tsx
+++ packages/web/src/pages/SignUp/index.tsx
@@ -1,10 +1,11 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useRef } from 'react';
 import {
   FiMail, FiLock, FiUser, FiArrowLeft,
 } from 'react-icons/fi';
-import * as Yup from 'yup';
-
 import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
+import getValidationErrors from '../../utils/getValidationErrors';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -14,8 +15,13 @@ import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 
 const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  // eslint-disable-next-line @typescript-eslint/ban-types
   const handleSubmit = useCallback(async (data: object) => {
     try {
+      formRef.current?.setErrors({});
+
       const schema = Yup.object().shape({
         name: Yup.string().required('Name required'),
         email: Yup.string().required('E-mail required').email('Invalid e-mail'),
@@ -23,10 +29,11 @@ const SignUp: React.FC = () => {
       });
 
       await schema.validate(data, {
-        abortEarly: true,
+        abortEarly: false,
       });
     } catch (err) {
-      console.error(data);
+      const errors = getValidationErrors(err);
+      formRef.current?.setErrors(errors);
     }
   }, []);
 
@@ -36,7 +43,7 @@ const SignUp: React.FC = () => {
       <Content>
         <img src={logoImg} alt="GoBarber" />
 
-        <Form onSubmit={handleSubmit}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Sign Up</h1>
           <Input name="name" icon={FiUser} placeholder="Name" />
           <Input name="email" icon={FiMail} placeholder="E-mail" />
